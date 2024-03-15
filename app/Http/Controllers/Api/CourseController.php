4a38a7497277311d2ee23ab7015150c9ff3268bb
milenatavares <milenatavares@lampp-it.com.br>
2024-03-15 15:35:33 -0300

    Aula 04 - Laravel API - Criar repository do curso

--- app/Http/Controllers/Api/CourseController.php
+++ app/Http/Controllers/Api/CourseController.php
@@ -3,23 +3,26 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Course;
 use App\Http\Resources\CourseResource;
+use App\Repositories\Courserepository;
 use Illuminate\Http\Request;
 
 class CourseController extends Controller
 {
+    protected $repository;
+
+    public function __construct(Courserepository $courseRepository)
+    {
+        $this->repository = $courseRepository;
+    }
+
     public function index()
     {
-        $courses = Course::get();
-
-        return CourseResource::collection($courses);
+        return CourseResource::collection($this->repository->getAllCourses());
     }
 
     public function show($id)
     {
-        $course = Course::findOrFail($id);
-
-        return new CourseResource($course);
+        return new CourseResource($this->repository->getCourse($id));
     }
 }
